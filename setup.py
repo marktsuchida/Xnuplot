5750b54467058fff27d559b1af619d60e621bca6
Mark A. Tsuchida <mark@tsuchida.org>
2010-06-09 15:22:31 -0700

    Distutils setup.py script.
    Version set to "0.1".

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='xnuplot',
+        version='0.1',
+        description='Pexpect-based adaptor for Gnuplot',
+        author='Mark A. Tsuchida',
+        author_email='mark@tsuchida.org',
+        py_modules=['xnuplot'],
+        )
+

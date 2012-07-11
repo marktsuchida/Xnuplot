2ec2745d6180b7b6c040c40392b8e5a10cff7624
Mark A. Tsuchida <mark@tsuchida.org>
2012-07-10 18:30:50 -0700

    Update copyright years where applicable.

--- xnuplot/numutils.py
+++ xnuplot/numutils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 Mark A. Tsuchida
+# Copyright (c) 2011-2012 Mark A. Tsuchida
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
